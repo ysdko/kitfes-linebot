1bb08c38396ae23f808a8e66d2a214449fb8b5ca
ysdko <koki.yoshida110@gmail.com>
2020-07-02 19:53:53 +0900

    Improve

--- index.js
+++ index.js
@@ -19,5 +19,26 @@ server.listen(process.env.PORT || 3000);
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 	res.sendStatus(200);
-	console.log(req.body);
+
+	let events_processed = [];
+
+	req.body.events.forEach((event) => {
+		if (event.type == "message" && event.message.type == "text") {
+			
+			if (event.message.text == "こんにちは") {
+				events_processed.push(bot.replyMessage(event.replyToken, {
+					type: "text",
+					text: "これはこれは"
+				}));
+			}
+		}
+	});
+
+
+	Promise.all(events_processed).then(
+		(response) => {
+			console.log(`${response.length} event(s) processed.`);
+		}
+	);
 });
+
