871b143c85b498df220a1fd5a6da3b40581e8e75
ysdko <koki.yoshida110@gmail.com>
2020-07-12 15:32:54 +0900

    Improve

--- index.old.js
+++ index.old.js
@@ -3,6 +3,7 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 const dialogflow = require("dialogflow");
+
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
@@ -25,16 +26,20 @@ const session_client = new dialogflow.SessionsClient({
 		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
 	}
 });
+
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+	// 先行してLINE側にステータスコード200でレスポンスする。
 	res.sendStatus(200);
 
+	// すべてのイベント処理のプロミスを格納する配列。
 	let events_processed = [];
 
+	// イベントオブジェクトを順次処理。
 	req.body.events.forEach((event) => {
+		// この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
 		if (event.type == "message" && event.message.type == "text") {
-			
 			events_processed.push(
 				session_client.detectIntent({
 					session: session_client.sessionPath(process.env.GOOGLE_PROJECT_ID, event.source.userId),
@@ -62,11 +67,10 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 		}
 	});
 
-
+	// すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
 	Promise.all(events_processed).then(
 		(response) => {
 			console.log(`${response.length} event(s) processed.`);
 		}
 	);
 });
-
