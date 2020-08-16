ac8db19cdbb85ed6a6ea085a165b6663a55ad27f
ysdko <koki.yoshida110@gmail.com>
2020-08-16 13:45:13 +0900

    improved

--- index.js
+++ index.js
@@ -1,24 +1,103 @@
-"use strict";
+// "use strict";
 
-/** 
- * Import Packages
- */
-const server = require("express")();
-const bot_express = require("bot-express");
+// /** 
+//  * Import Packages
+//  */
+// const server = require("express")();
+// const bot_express = require("bot-express");
 
-/** 
- * Middleware Configuration
- */
-server.listen(process.env.PORT || 5000, () => {
-    console.log("server is running...");
-});
+// /** 
+//  * Middleware Configuration
+//  */
+// server.listen(process.env.PORT || 5000, () => {
+//     console.log("server is running...");
+// });
 
-/** 
- * Mount bot-express
- */
+// /** 
+//  * Mount bot-express
+//  */
+
+// server.use("/bot/webhook", bot_express({
+//     language: "ja",
+//     messenger: {
+//         line: {
+//             // channel_id: process.env.LINE_CHANNEL_ID,
+//             // channel_secret: process.env.LINE_CHANNEL_SECRET
+//             channel_id: '2f13a44596ce0204276a12c9f3d31630',
+//             channel_secret: '7OTzSy6u/t52SwHF8NdvnLsKoDIds+GeR+vA17TvJggY+EyHyQdUvkBO2/yAJlwn+nYJpKnb9U6B9GKHhmfaj7r/DPfH5FgQylgDelu9khHEvHdDYmGD3dQEjZI0s83bn8YVOk0WH4sqnsXiDTdXkAdB04t89/1O/w1cDnyilFU='
+//         }
+//     },
+//     nlu: {
+//         type: "dialogflow",
+//         options: {
+//             project_id: process.env.GOOGLE_PROJECT_ID,
+//             client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//             private_key: process.env.GOOGLE_PRIVATE_KEY,
+//             language: "ja"
+//         }
+//     },
+//     parser: [{
+//         type: "dialogflow",
+//         options: {
+//             project_id: process.env.GOOGLE_PROJECT_ID,
+//             client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//             private_key: process.env.GOOGLE_PRIVATE_KEY,
+//             language: "ja"
+//         }
+//     }],
+//     memory: {
+//         type: process.env.MEMORY_TYPE || "memory-cache", // memory-cache | redis
+//         retention: Number(process.env.MEMORY_RETENTION),
+//         options: {
+//             url: process.env.REDIS_URL
+//         }
+//     },
+//     translator: {
+//         type: "google",
+//         enable_lang_detection: false,
+//         enable_translation: false,
+//         options: {
+//             project_id: process.env.GOOGLE_PROJECT_ID,
+//             client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//             private_key: process.env.GOOGLE_PRIVATE_KEY
+//         }
+//     },
+//     logger: {
+//         type: process.env.LOGGER_TYPE || "stdout", // stdout | firestore
+//         options: { // Options for firestore.
+//             project_id: process.env.FIREBASE_PROJECT_ID,
+//             client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//             private_key: process.env.FIREBASE_PRIVATE_KEY,
+//         }
+//     },
+//     skill: {
+//         default: process.env.DEFAULT_SKILL
+//     }
+// }));
+
+// module.exports = server;
+
+const express = require('express'); // expressインポート
+const app = express();
+const line = require('@line/bot-sdk');// sdkインポート
+
+const config = {
+    channelAccessToken: process.env.BANGOHAN_RECOMMEND_ACCESS_TOKEN, // 環境変数からアクセストークンをセット
+    channelSecret: process.env.BANGOHAN_RECOMMEND_SECRET_KEY // 環境変数からChannel Secretをセット
+};
+const client = new line.Client(config); // APIコールのためのクライアントインスタンスを作成
+
+const port = process.env.PORT || 3000; // ポートを環境変数or3000で設定
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// WebHookのエンドポイント
+app.post('/hook', line.middleware(config), (req, res) => lineBot(req, res));
+
 const bangohanList = ["SAINO", "章吉", "もっちゃん", "なかよし", "学食", "夕月庵", "チャイナ", "コンビニ", "自炊"];
 // line bot 本体
 function lineBot(req, res) {
+    res.status(200).end(); // すぐにLINE側にステータスコード200でレスポンス
+    // チャットボットの処理をかいていく
     res.status(200).end(); // すぐにLINE側にステータスコード200でレスポンス
     const promises = []; // すべてのイベント処理のpromiseを格納する配列
     const events = req.body.events; // イベントオブジェクト
@@ -83,62 +162,3 @@ function lineBot(req, res) {
     // 全プロミスを処理したらログを出力
     Promise.all(promises).then(console.log(`promise completed\n`));
 }
-server.use("/bot/webhook", bot_express({
-    language: "ja",
-    messenger: {
-        line: {
-            // channel_id: process.env.LINE_CHANNEL_ID,
-            // channel_secret: process.env.LINE_CHANNEL_SECRET
-            channel_id: '2f13a44596ce0204276a12c9f3d31630',
-            channel_secret: '7OTzSy6u/t52SwHF8NdvnLsKoDIds+GeR+vA17TvJggY+EyHyQdUvkBO2/yAJlwn+nYJpKnb9U6B9GKHhmfaj7r/DPfH5FgQylgDelu9khHEvHdDYmGD3dQEjZI0s83bn8YVOk0WH4sqnsXiDTdXkAdB04t89/1O/w1cDnyilFU='
-        }
-    },
-    nlu: {
-        type: "dialogflow",
-        options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
-            language: "ja"
-        }
-    },
-    parser: [{
-        type: "dialogflow",
-        options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
-            language: "ja"
-        }
-    }],
-    memory: {
-        type: process.env.MEMORY_TYPE || "memory-cache", // memory-cache | redis
-        retention: Number(process.env.MEMORY_RETENTION),
-        options: {
-            url: process.env.REDIS_URL
-        }
-    },
-    translator: {
-        type: "google",
-        enable_lang_detection: false,
-        enable_translation: false,
-        options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY
-        }
-    },
-    logger: {
-        type: process.env.LOGGER_TYPE || "stdout", // stdout | firestore
-        options: { // Options for firestore.
-            project_id: process.env.FIREBASE_PROJECT_ID,
-            client_email: process.env.FIREBASE_CLIENT_EMAIL,
-            private_key: process.env.FIREBASE_PRIVATE_KEY,
-        }
-    },
-    skill: {
-        default: process.env.DEFAULT_SKILL
-    }
-}));
-
-module.exports = server;
